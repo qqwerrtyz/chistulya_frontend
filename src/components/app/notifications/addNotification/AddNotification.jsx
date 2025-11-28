99f1dbeba78e161df86c0b5535b3cfdd025a72a7
Alex <mr.unnona@gmail.com>
2025-11-28 10:12:45 +0300

    _

--- src/components/app/notifications/addNotification/AddNotification.jsx
+++ src/components/app/notifications/addNotification/AddNotification.jsx
@@ -2,8 +2,15 @@ import Image from "next/image"
 import styles from "./../Notifications.module.css"
 import icons from "../../../../../public/icons/icons"
 import GeneralParamsInput from "./generalParamsInput/GeneralParamsInput"
+import { useState } from "react"
 export default function AddNotification({setShowAddNotification}) {
-    
+    const [dataNotification, setDataNotification] = useState({
+        type: null,
+        time: null,
+        interval: null,
+        title: null,
+        subTitle: null
+    })
 
     return (
         <div className={styles.addNotificationWrapper}>
@@ -26,7 +33,7 @@ export default function AddNotification({setShowAddNotification}) {
                         </div>
 
                         <div className={styles.generalParamsFieldsWrapper}>
-                            <GeneralParamsInput />
+                            <GeneralParamsInput dropdown={true}/>
 
                             
                         </div>
