1792d64b9b2d8adc80ca222398ad6e8edaa89211
qqwerrtyz <mr.unnona@gmail.com>
2026-08-22 13:37:58 +0300

    loc var для разработки на vercel

--- src/config/publicEnv.js
+++ src/config/publicEnv.js
@@ -1,17 +1,58 @@
-const graphqlUrl = process.env.NEXT_PUBLIC_GRAPHQL_URL
-const appUrl = process.env.NEXT_PUBLIC_APP_URL
+// // const graphqlUrl = process.env.NEXT_PUBLIC_GRAPHQL_URL
+// // const appUrl = process.env.NEXT_PUBLIC_APP_URL
 
-if (!graphqlUrl) {
-    throw new Error(
-        "Не задана переменная окружения NEXT_PUBLIC_GRAPHQL_URL"
-    )
-}
+// // if (!graphqlUrl) {
+// //     throw new Error(
+// //         "Не задана переменная окружения NEXT_PUBLIC_GRAPHQL_URL"
+// //     )
+// // }
 
-if (!appUrl) {
-    throw new Error(
-        "Не задана переменная окружения NEXT_PUBLIC_APP_URL"
+// // if (!appUrl) {
+// //     throw new Error(
+// //         "Не задана переменная окружения NEXT_PUBLIC_APP_URL"
+// //     )
+// // }
+
+// // export const GRAPHQL_URL = graphqlUrl.replace(/\/+$/, "")
+// // export const APP_URL = appUrl.replace(/\/+$/, "")
+
+// const graphqlUrl = process.env.NEXT_PUBLIC_GRAPHQL_URL
+// const appUrl = process.env.NEXT_PUBLIC_APP_URL
+
+// if (!graphqlUrl) {
+//     throw new Error(
+//         "Не задана переменная окружения NEXT_PUBLIC_GRAPHQL_URL"
+//     )
+// }
+
+// if (!appUrl) {
+//     throw new Error(
+//         "Не задана переменная окружения NEXT_PUBLIC_APP_URL"
+//     )
+// }
+
+// export const GRAPHQL_URL = graphqlUrl.replace(/\/+$/, "")
+// export const APP_URL = appUrl.replace(/\/+$/, "")
+
+
+const DEFAULT_GRAPHQL_URL =
+    "https://api.xn--h1agrefu5d.xn--p1ai/graphql"
+
+const DEFAULT_APP_URL =
+    "https://xn--h1agrefu5d.xn--p1ai"
+
+const graphqlUrl =
+    process.env.NEXT_PUBLIC_GRAPHQL_URL ||
+    DEFAULT_GRAPHQL_URL
+
+const appUrl =
+    process.env.NEXT_PUBLIC_APP_URL ||
+    (
+        typeof window !== "undefined"
+            ? window.location.origin
+            : DEFAULT_APP_URL
     )
-}
 
 export const GRAPHQL_URL = graphqlUrl.replace(/\/+$/, "")
+
 export const APP_URL = appUrl.replace(/\/+$/, "")
